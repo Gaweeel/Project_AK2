c4325093b155bd761d598e0e32f6ba4e1574cb01
Kamil <kamilw6969@gmail.com>
2023-06-05 19:41:36 +0200

    fixed CmakeLists

---
+++ Google_tests/stack_vm_tests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kamil on 05.06.2023.
+//
+#include "gtest/gtest.h"
+#include "stack_vm/stack_vm.h"
+
+TEST(LexerTest, SimpleTest){
+	EXPECT_EQ(true, true);
+}
+TEST(LexerTest, SimpleTest2){
+	EXPECT_EQ(true, true);
+}
