345245c0ca9a64bbecbbe126f140431d1602ba59
Kamil <kamilw6969@gmail.com>
2023-06-05 18:50:28 +0200

    changed build system to cmake and added google tests

---
+++ Google_tests/lexer_tests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kamil on 05.06.2023.
+//
+#include "gtest/gtest.h"
+#include "sasm/lexer.h"
+
+TEST(LexerTest, SimpleTest){
+	EXPECT_EQ(true, true);
+}
+TEST(LexerTest, SimpleTest2){
+	EXPECT_EQ(true, true);
+}
---
+++ src/stack_vm/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+//#include "VirtualMachine.h"
+#include "stack_vm.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+   if (argc < 2) {
+        cout << "Usage message: " << argv[0] << " <filename>" << endl;
+        return 0;
+    }
+
+    ifstream r(argv[1], ios::binary);
+    sint i;
+    vector<sint> program;
+
+    while (r.read((char*)&i, sizeof(i))) {
+        program.push_back(i);
+    }
+
+    VirtualMachine VM;
+    //std::vector<sint> program{21, 37, 0x40000001, 8,0x40000002,12,0x40000003,5,0x40000004,0x40000000};
+    VM.load(program);
+    VM.run();
+    return 0;
+}
